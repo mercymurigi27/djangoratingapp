68fefd6e6a4413de75040b149453969cdcf655eb
mercymurigi27 <mercy.murigi@student.moringaschool.com>
2022-06-15 01:13:23 +0300

    add serialization

---
+++ award/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import  serializers
+from .models import Post, Profile,User
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['username','email']
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Profile
+        fields = ['prof_pic','bio','contact','name','user']
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ['title','photo','description','url','date']
+
